fc9b98746174a228d9721be07d8e11e02a38a1a6
quanbigboss1 <82883115+quanbigboss1@users.noreply.github.com>
2022-11-29 16:41:18 +0700

    Add files via upload

---
+++ lab5_bai2sql.sql
@@ -0,0 +1,55 @@
+﻿--cau a
+create proc bai2a @MANV varchar(3)
+as
+begin
+	select * from NHANVIEN where MANV = @MANV
+end
+exec bai2a '009'
+-- cau b
+create proc bai2_b @MANV int
+as
+begin
+	select count(MANV) as 'so luong' ,MADA,TENPHG FROM NHANVIEN
+	inner join PHONGBAN ON NHANVIEN.PHG = PHONGBAN.MAPHG
+	inner join DEAN ON DEAN.PHONG = PHONGBAN.MAPHG
+	where MADA = @manv
+	group by TENPHG,MADA
+end
+-- cau c
+go
+CREATE PROCEDURE BAI2_c @MaDA varchar(3),@Ddiem_DA nvarchar(50)
+as
+	begin
+	select count(MANV) as 'so luong', MADA,DDIEM_DA from NHANVIEN
+	INNER JOIN PHONGBAN ON NHANVIEN.PHG=PHONGBAN.MAPHG
+	INNER JOIN DEAN ON DEAN.PHONG=PHONGBAN.MAPHG
+	WHERE MADA=@MaDA and DDIEM_DA=@Ddiem_DA
+	group by MADA,DDIEM_DA
+	end
+go
+go
+exec BAI2_c 10,N'Hà Nội'
+go
+-- cau d
+create proc bai2_d @MATP varchar(5)
+as
+begin
+	select HONV,TENNV,TENPHG,NHANVIEN.MANV,THANNHAN.*
+	from NHANVIEN
+	inner join PHONGBAN on PHONGBAN.MAPHG = NHANVIEN.PHG
+	left outer join THANNHAN on THANNHAN.MA_NVIEN = NHANVIEN.MANV
+	where THANNHAN.MA_NVIEN is null and TRPHG = @MATP
+end
+exec bai2_d '008'
+-- cau e
+create proc bai2_e @MANV varchar(5),@MAPB varchar(5)
+as
+begin
+	if exists(select * from NHANVIEN where MANV = @MANV and PHG = @MAPB)
+	print 'NHAN VIEN : '+@MANV +' co trong phong ban : '+@MaPB
+else
+	print('NHAN VIEN : '+@MANV +' khong co trong phong ban : '+@MaPB
+end
+exec bai2_e '001'
+
+
---
+++ lab5_bai3.sql
@@ -0,0 +1,63 @@
+﻿create proc ThemPhongBanMoi
+	@TenPhg nvarchar(20),@MaPhg int,@TrPhg nvarchar(10), @Ng_NhanChuc date
+as
+begin
+	if exists(select * from PHONGBAN where MAPHG = @MAPHG)
+	begin
+		print('Mã phòng ban đã tồn tại');
+		return;
+	end
+
+	Insert into [dbo].[PHONGBAN]
+		([TENPHG],[MAPHG],[TRPHG],[NG_NHANCHUC]
+	Values
+		(@TenPhg,@MaPhg,@TenPhg,@Ng_NhanChuc);
+end
+exec ThemPhongBanMoi 'IT','11','005','2022-11-29'
+-------
+create proc sp_CapNhatPhongBan
+	@OldTenPHG nvarchar(15),
+	@TenPHG nvarchar(15),
+	@MaPHG int,
+	@TRPHG nvarchar(10),
+	@NG_NHANCHUC date
+as
+begin
+	UPDATE [dbo].[PHONGBAN]
+	SET 
+		[TENPHG] =@TENPHG,
+		[MAPHG] = @MAPHG,
+		[TRPHG] = @TRPHG,
+		[NG_NHANCHUC]=@NG_NHANCHUC
+		where TENPHG = @OldTenPHG;
+end;
+exec [dbo].[sp_CapNhatPhongBan] 'CNTT','IT','10','005','1-1-2020'
+---------------------------------------------
+create proc sp_insertNV @Ho nvarchar(15),@tenNV nvarchar(15),@MaNV nvarchar(9),@NgaySinh datetime,@diachi nvarchar(30),@phai nvarchar(3),@luong float,@Ma_NQL nvarchar(9),@PHG int
+as
+begin
+	if not exists(select * from PHONGBAN where TENPHG like 'IT')
+	begin
+		print 'NHAN VIEN phai la phong IT'
+	return
+end
+if @luong < 25000
+set @Ma_NQL='009'
+else
+begin
+set @Ma_NQL = '005'
+end
+declare @age int = datediff(YEAR,@ngaySinh,getDate())
+if(@phai like 'nam' and @age > 65 and @age <18)
+begin
+print 'nam phai tu 18 - 65 '
+return 
+end
+else if(@phai like N'Nữ' and @age > 60 and @age < 18)
+begin
+print 'nu phai tu 18-60'
+return 
+end
+insert into NHANVIEN(HONV,TENLOT,TENNV,MANV,NGSINH,DCHI,PHAI,LUONG,MA_NQL,PHG)
+values(@Ho,@tenNV,@MaNV,@NgaySinh,@diachi,@phai,@luong,@Ma_NQL,@PHG)
+end
