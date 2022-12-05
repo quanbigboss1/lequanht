b80b28797049dd880f4090bdda8ce083e318fb65
quanbigboss1 <82883115+quanbigboss1@users.noreply.github.com>
2022-12-05 13:32:45 +0700

    Add files via upload

---
+++ Query_QLBH(8-15).sql
@@ -0,0 +1,57 @@
+﻿--8.	In ra danh sách các khách hàng (MAKH, HOTEN) đã mua hàng trong ngày 1/1/2007.
+SELECT K.MAKH, HOTEN
+FROM KHACHHANG K INNER JOIN HOADON H
+ON K.MAKH = H.MAKH
+WHERE NGHD = '1/1/2007'
+--9.	In ra số hóa đơn, trị giá các hóa đơn do nhân viên có tên “Nguyen Van B” lập trong ngày 28/10/2006.
+SELECT SOHD, TRIGIA
+FROM HOADON H INNER JOIN NHANVIEN N
+ON H.MANV = N.MANV
+WHERE NGHD = '10/28/2006'
+AND HOTEN = 'NGUYEN VAN B'
+--10.	In ra danh sách các sản phẩm (MASP,TENSP) được khách hàng có tên “Nguyen Van A” mua trong tháng 10/2006.
+SELECT DISTINCT S.MASP, TENSP
+FROM SANPHAM S INNER JOIN CTHD C
+ON S.MASP = C.MASP
+AND EXISTS(SELECT *
+FROM CTHD C INNER JOIN HOADON H
+ON C.SOHD = H.SOHD
+AND MONTH(NGHD) = 10 AND YEAR(NGHD) = 2006 AND MAKH IN(SELECT H.MAKH
+FROM HOADON H INNER JOIN KHACHHANG K
+ON H.MAKH = K.MAKH
+WHERE HOTEN = 'NGUYEN VAN A') AND S.MASP = C.MASP)
+--11.	Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”.
+SELECT SOHD
+FROM CTHD
+WHERE MASP IN ('BB01', 'BB02')
+--12.	Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
+SELECT SOHD
+FROM CTHD
+WHERE MASP IN ('BB01', 'BB02')
+AND SL BETWEEN 10 AND 20
+--13.	Tìm các số hóa đơn mua cùng lúc 2 sản phẩm có mã số “BB01” và “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
+SELECT SOHD
+FROM CTHD A
+WHERE A.MASP = 'BB01'
+AND SL BETWEEN 10 AND 20
+AND EXISTS(SELECT *
+FROM CTHD B
+WHERE B.MASP = 'BB02'
+AND SL BETWEEN 10 AND 20
+AND A.SOHD = B.SOHD)
+--14.	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất hoặc các sản phẩm được bán ra trong ngày 1/1/2007.
+SELECT DISTINCT S.MASP, TENSP
+FROM SANPHAM S INNER JOIN CTHD C
+ON S.MASP = C.MASP
+WHERE NUOCSX = 'TRUNG QUOC'
+AND C.SOHD IN(SELECT DISTINCT C2.SOHD
+FROM CTHD C2 INNER JOIN HOADON H
+ON C2.SOHD = H.SOHD
+WHERE NGHD ='1/1/2007')
+--15.	In ra danh sách các sản phẩm (MASP,TENSP) không bán được.
+SELECT S.MASP, TENSP
+FROM SANPHAM S
+WHERE NOT EXISTS(SELECT * 
+FROM SANPHAM S2 INNER JOIN CTHD C
+ON S2.MASP = C.MASP
+AND S2.MASP = S.MASP)
